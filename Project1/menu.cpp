6c244849ff2817c098188aa1ca9b807455477561
GustavoCMadureira <up202304978@up.pt>
2025-03-26 23:12:42 +0000

    Menu now does not crash when inputting invalid choice

--- Project1/menu.cpp
+++ Project1/menu.cpp
@@ -21,10 +21,10 @@ void writeInputToFile(int choice, int source, int destination, int maxWalkTime,
         return;
     }
 
-    if (choice == 1 || choice ==2) {
+    if (choice == 1 || choice == 2) {
         inputFile << "Mode:driving\n";
     }
-    else if (choice ==3 ){
+    else if (choice == 3){
         inputFile << "Mode:driving-walking\n";
     }
 
@@ -39,7 +39,7 @@ void writeInputToFile(int choice, int source, int destination, int maxWalkTime,
     inputFile << "IncludeNode:" << (includeNode == -1 ? "none" : std::to_string(includeNode)) << "\n";
 
     inputFile.close();
-    std::cout << "Data successfully saved in input.txt!\n";
+    std::cout << "\nData successfully saved in input.txt!\n";
 }
 
 // Function to get a valid integer (positive)
@@ -81,15 +81,12 @@ std::string getValidList(const std::string& prompt, const std::string& pattern) 
     }
 }
 
-void receiveInput(int choice){
-
+void receiveInput(const int choice){
     int source = -1, destination = -1, maxWalkTime = -1, includeNode = -1;
     std::string avoidNodes, avoidSegments;
 
-    if (choice == 1 || choice == 2 || choice == 3) {
-        source = getValidInt("Source: ");
-        destination = getValidInt("Destination: ");
-    }
+    source = getValidInt("Source: ");
+    destination = getValidInt("Destination: ");
 
     if (choice == 2) {
         avoidNodes = getValidList("AvoidNodes (e.g., 1,2,3 or empty): ", R"(^\d+(,\d+)*$|^$)");
@@ -104,21 +101,30 @@ void receiveInput(int choice){
     writeInputToFile(choice, source, destination, maxWalkTime, avoidNodes, avoidSegments, includeNode);
 }
 
-void selectMode(int choice){
-    int mode;
-    std::cout << "\n===== Mode Selection =====\n";
-    std::cout << "1. Manual Input\n";
-    std::cout << "2. Automatic Input\n";
-    std::cout << "Enter your choice: ";
-    std::cin >> mode;
+int selectMode(const int choice){
+    int mode = -1;
+    do {
+        std::cout << "\n===== Mode Selection =====\n";
+        std::cout << "1. Manual Input\n";
+        std::cout << "2. Automatic Input\n";
+        std::cout << "3. Back\n";
+        std::cout << "Enter your choice: ";
+        std::cin >> mode;
 
-    if(mode==1){
-        receiveInput(choice);
-    }
-    if(mode>2){
-        std::cout<< "\nInvalid Choice\n";
-        std::cout << "Enter a valid choice: ";
-    }
+        switch (mode) {
+            case 1:
+                receiveInput(choice);
+            break;
+            case 3:
+                return 1;
+            default:
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std::cout << "Invalid choice. Please try again.\n";
+            break;
+        }
+    } while (mode > 3 || mode < 1);
+    return 0;
 }
 
 int main() {
@@ -140,21 +146,23 @@ int main() {
 
         switch (choice) {
             case 1:
-                selectMode(choice);
+                if (selectMode(choice)) break; // if user goes back from the other menu do not execute the algorithm
                 independentRoutePlanning(urbanGraph);
             break;
             case 2:
-                selectMode(choice);
+                if (selectMode(choice)) break;
                 restrictedRoutePlanning(urbanGraph);
             break;
             case 3:
-                selectMode(choice);
+                if (selectMode(choice)) break;
                 environmentallyFriendlyRoutePlanning(urbanGraph);
             break;
             case 4:
                 std::cout << "Exiting program.\n";
             break;
             default:
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
                 std::cout << "Invalid choice. Please try again.\n";
             break;
         }
